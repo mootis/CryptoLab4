5a71e70f43eb9c7e89e66913180453fcc7cfb2a0
chong050 <chong050@morris.umn.edu>
2018-12-04 14:02:43 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        int answer = emod(5, 213962984, 658996103);
+        System.out.println("Our A: " + answer);
+        answer = emod(2, 28406022, 35530787);
+        System.out.println("Our B: " + answer);
+        answer = emod(28406022, 213962984, 35530787);
+        System.out.println("Our Alice key: " + answer);
+        answer = emod(213962984,28406022,658996103);
+        System.out.println("Our Bob key: " + answer);
+        */
+        int answer;
+        answer = emod(2,1234,35530787);
+        System.out.println("Our A: " + answer);
+        answer = emod(5,5678,658996103);
+        System.out.println("Our B: " + answer);
+        answer = emod(213962984,5678,658996103);
+        System.out.println(answer);
+        answer = emod(28406022,1234,35530787);
+        System.out.println(answer);
+    }
+
+    public static int emod(int a, int b, int c) {
+        if (b == 0) {
+            return 1;
+        } else if (b % 2 == 0) {
+            int d = emod(a, b / 2, c);
+            return ((d * d) % c);
+        } else {
+            return (((a % c) * emod(a, b - 1, c)) % c);
+        }
+    }
+}
